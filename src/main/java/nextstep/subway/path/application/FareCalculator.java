6a88fc8404ac27f27d0943c914a27db8cef07df1
Giwan Kim <giwan.kim@gmail.com>
2024-08-04 16:21:58 +0900

    feat(path): 경로로부터 요금을 계산하는 클래스를 추가했다.

---
+++ src/main/java/nextstep/subway/path/application/FareCalculator.java
@@ -0,0 +1,22 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.path.domain.Path;
+
+public class FareCalculator {
+  private static final long BASE_FARE = 1250L;
+  private static final long ZERO = 0L;
+
+  public long calculateFare(Path path) {
+    return BASE_FARE + calculateOverFare(path.getTotalDistance());
+  }
+
+  private long calculateOverFare(long distance) {
+    if (distance <= 10) {
+      return ZERO;
+    }
+    if (distance <= 50) {
+      return (long) Math.ceil((double) (distance - 10) / 5) * 100L;
+    }
+    return 800L + (long) Math.ceil((double) (distance - 50) / 8) * 100L;
+  }
+}
