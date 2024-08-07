67e299b76fe2fcf1a0686b3ca41bea791a30a6e1
Giwan Kim <giwan.kim@gmail.com>
2024-08-07 11:09:15 +0900

    test(surcharge): 추가 요금 생성 인수 테스트를 작성했다.

---
+++ src/test/java/nextstep/cucumber/steps/SurchargeStepDefinitions.java
@@ -0,0 +1,37 @@
+package nextstep.cucumber.steps;
+
+import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
+import java.util.List;
+import java.util.Map;
+import nextstep.cucumber.support.AcceptanceContext;
+import nextstep.subway.line.application.dto.LineResponse;
+import nextstep.subway.surcharge.application.dto.SurchargeRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class SurchargeStepDefinitions {
+  @Autowired private AcceptanceContext context;
+
+  @Given("노선별 추가 요금을 등록하고")
+  public void 노선별_추가_요금을_등록하고(List<Map<String, String>> rows) {
+    rows.forEach(
+        it -> {
+          Long lineId = ((LineResponse) context.store.get(it.get("line"))).getId();
+          Long surcharge = Long.parseLong(it.get("surcharge"));
+          SurchargeRequest request = new SurchargeRequest(lineId, surcharge);
+          RestAssured.given()
+              .log()
+              .all()
+              .contentType(MediaType.APPLICATION_JSON_VALUE)
+              .body(request)
+              .when()
+              .post("/surcharges")
+              .then()
+              .log()
+              .all()
+              .extract();
+        });
+  }
+}
