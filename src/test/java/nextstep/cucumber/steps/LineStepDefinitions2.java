515f954e892392d874e04cbdcea03da217a535ad
Giwan Kim <giwan.kim@gmail.com>
2024-08-11 08:27:01 +0900

    test(fare): 인수 테스트 리팩링된 경로를 상요도록 인수 테스트를 수정했다.

--- src/test/java/nextstep/cucumber/steps/LineStepDefinitions2.java
+++ src/test/java/nextstep/cucumber/steps/LineStepDefinitions2.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import java.util.List;
 import java.util.Map;
@@ -15,11 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 @SuppressWarnings("NonAsciiCharacters")
-public class LineStepDefinitions {
+public class LineStepDefinitions2 {
   @Autowired private AcceptanceContext context;
 
-  @Given("구간들을 등록하고")
-  public void 구간들을_등록하고(List<Map<String, String>> rows) {
+  @Given("구간들을 등록하고2")
+  public void 구간들을_등록하고2(List<Map<String, String>> rows) {
     rows.forEach(
         it -> {
           Long upStationId = ((StationResponse) context.store.get(it.get("upStation"))).getId();
@@ -30,14 +29,14 @@ public class LineStepDefinitions {
                   downStationId,
                   Integer.parseInt(it.get("distance")),
                   Integer.parseInt(it.get("duration")));
-          LineResponse line = (LineResponse) context.store.get(it.get("line"));
+          LineResponse2 line = (LineResponse2) context.store.get(it.get("line"));
           RestAssured.given()
               .log()
               .all()
               .contentType(MediaType.APPLICATION_JSON_VALUE)
               .body(request)
               .when()
-              .post("/lines/" + line.getId() + "/sections")
+              .post("/new/lines/" + line.getId() + "/sections")
               .then()
               .log()
               .all()
@@ -45,21 +44,22 @@ public class LineStepDefinitions {
         });
   }
 
-  @When("노선들을 생성하고")
-  public void 노선들을_생성하고(List<Map<String, String>> rows) {
+  @Given("노선들을 생성하고2")
+  public void 노선들을_생성하고2(List<Map<String, String>> rows) {
     rows.forEach(
         row -> {
           Long upStationId = ((StationResponse) context.store.get(row.get("upStation"))).getId();
           Long downStationId =
               ((StationResponse) context.store.get(row.get("downStation"))).getId();
-          LineRequest request =
-              LineRequest.builder()
+          LineRequest2 request =
+              LineRequest2.builder()
                   .name(row.get("name"))
                   .color(row.get("color"))
                   .upStationId(upStationId)
                   .downStationId(downStationId)
                   .distance(Integer.parseInt(row.get("distance")))
                   .duration(Integer.parseInt(row.get("duration")))
+                  .surcharge(Integer.parseInt(row.get("surcharge")))
                   .build();
           var response =
               RestAssured.given()
@@ -68,21 +68,21 @@ public class LineStepDefinitions {
                   .body(request)
                   .contentType(MediaType.APPLICATION_JSON_VALUE)
                   .when()
-                  .post("/lines")
+                  .post("/new/lines")
                   .then()
                   .log()
                   .all()
                   .extract();
-          context.store.put(request.getName(), response.as(LineResponse.class));
+          context.store.put(request.getName(), response.as(LineResponse2.class));
         });
   }
 
-  @Then("지하철 노선 목록 조회 시 {string}을 찾을 수 있다")
-  public void 지하철_노선_목록_조회_시_생성한_노선을_찾을_수_있다(String line) {
+  @Then("지하철 노선 목록 조회 시 {string}을 찾을 수 있다2")
+  public void 지하철_노선_목록_조회_시_생성한_노선을_찾을_수_있다2(String line) {
     var response =
-        RestAssured.given().log().all().when().get("/lines").then().log().all().extract();
-    List<LineResponse> actualLines = response.jsonPath().getList(".", LineResponse.class);
-    LineResponse expectedLine = (LineResponse) context.store.get(line);
+        RestAssured.given().log().all().when().get("/new/lines").then().log().all().extract();
+    List<LineResponse2> actualLines = response.jsonPath().getList(".", LineResponse2.class);
+    LineResponse2 expectedLine = (LineResponse2) context.store.get(line);
     assertThat(actualLines).contains(expectedLine);
   }
 }
